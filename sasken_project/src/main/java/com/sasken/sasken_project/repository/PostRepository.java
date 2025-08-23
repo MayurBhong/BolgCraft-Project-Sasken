30f3bd55683f6a608533a348283ff262dc4f20b6
Mayur Bhong <155006076+MayurBhong@users.noreply.github.com>
2025-08-23 21:53:01 +0530

    Update PostRepository.java

--- sasken_project/src/main/java/com/sasken/sasken_project/repository/PostRepository.java
+++ sasken_project/src/main/java/com/sasken/sasken_project/repository/PostRepository.java
@@ -1,7 +1,6 @@
 package com.sasken.sasken_project.repository; // FIXED: Removed backslash
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
